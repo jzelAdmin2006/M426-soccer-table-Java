d5fac0ec14113bace16a3567f484b5c277f6fdf8
jzelAdmin <johannes_zeller@sluz.ch>
2023-02-22 13:51:52 +0100

    finish first version of project

--- SoccerTableStats/src/main/java/tech/bison/trainee2021/SoccerTableStats/SoccerTableStats.java
+++ SoccerTableStats/src/main/java/tech/bison/trainee2021/SoccerTableStats/SoccerTableStats.java
@@ -1,7 +1,35 @@
 package tech.bison.trainee2021.SoccerTableStats;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import tech.bison.trainee2021.SoccerTableStats.structure.GameResult;
+import tech.bison.trainee2021.SoccerTableStats.structure.League;
+
 public class SoccerTableStats {
+	private static final String INPUT_FILES_LOCATION = System.getenv("INPUT_FILES_LOCATION");
+	private static final String OUTPUT_FILES_LOCATION = System.getenv("OUTPUT_FILES_LOCATION");
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		new SoccerTableStats().writeOutputByInput();
+	}
+
+	private void writeOutputByInput() {
+		File[] inputFiles = new File(INPUT_FILES_LOCATION).listFiles();
+		for (File file : inputFiles) {
+			try {
+				League league = new League();
+				league.addGameResults(GameResult.parseGameResults(Files.readString(Path.of(file.getAbsolutePath()))));
+				FileOutputStream outputStream = new FileOutputStream(
+						Path.of(OUTPUT_FILES_LOCATION).resolve(file.getName()).toString());
+				outputStream.write(league.toString().getBytes());
+				outputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
