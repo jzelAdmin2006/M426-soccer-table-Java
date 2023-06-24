9a7a04e12219d4522dce2554c36c2cf2a08a8d52
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-06-24 16:25:40 +0200

    fulfil convention: utility classes should not have public constructors

--- SoccerTableStats/src/main/java/tech/bison/trainee2021/SoccerTableStats/util/StreamUtils.java
+++ SoccerTableStats/src/main/java/tech/bison/trainee2021/SoccerTableStats/util/StreamUtils.java
@@ -5,6 +5,10 @@ import java.util.List;
 import java.util.function.Function;
 
 public class StreamUtils {
+	private StreamUtils() {
+		throw new IllegalStateException("Utility class");
+	}
+
 	public static <T, R> List<R> mapToList(Collection<T> collection, Function<T, R> mapper) {
 		return collection.stream().map(mapper).toList();
 	}
