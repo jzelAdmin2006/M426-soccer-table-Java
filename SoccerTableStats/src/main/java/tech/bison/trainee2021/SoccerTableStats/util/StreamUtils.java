32c080c4c3496ef2b9dc444eac6783e7684f2c25
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-06-24 16:18:15 +0200

    improve conciseness

--- SoccerTableStats/src/main/java/tech/bison/trainee2021/SoccerTableStats/util/StreamUtils.java
+++ SoccerTableStats/src/main/java/tech/bison/trainee2021/SoccerTableStats/util/StreamUtils.java
@@ -1,13 +1,11 @@
 package tech.bison.trainee2021.SoccerTableStats.util;
 
-import static java.util.stream.Collectors.toList;
-
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
 public class StreamUtils {
 	public static <T, R> List<R> mapToList(Collection<T> collection, Function<T, R> mapper) {
-		return collection.stream().map(mapper).collect(toList());
+		return collection.stream().map(mapper).toList();
 	}
 }
