9d10954961c89f2efb8af717e5490e975f8adadb
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-06-24 17:20:14 +0200

    provide logging information

--- SoccerTableStats/src/main/java/tech/bison/trainee2021/soccer_table_stats/SoccerTableStats.java
+++ SoccerTableStats/src/main/java/tech/bison/trainee2021/soccer_table_stats/SoccerTableStats.java
@@ -8,12 +8,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import tech.bison.trainee2021.soccer_table_stats.structure.business.League;
 
 public class SoccerTableStats {
 	private static final String INPUT_FILES_LOCATION = validateGetEnv("INPUT_FILES_LOCATION");
 	private static final String OUTPUT_FILES_LOCATION = validateGetEnv("OUTPUT_FILES_LOCATION");
+	private static final Logger LOGGER = Logger.getLogger(SoccerTableStats.class.getName());
 	public static final String SOCCER_TABLE_STATS_FILE_EXTENSION = ".scrts";
 
 	public static void main(String[] args) {
@@ -23,14 +26,15 @@ public class SoccerTableStats {
 	private void writeOutputByInput() {
 		File[] inputFiles = new File(INPUT_FILES_LOCATION).listFiles();
 		for (File file : inputFiles) {
-			try (FileOutputStream outputStream = new FileOutputStream(
-					Path.of(OUTPUT_FILES_LOCATION).resolve(file.getName()).toString()
-							+ SOCCER_TABLE_STATS_FILE_EXTENSION)) {
+			String destinationPath = Path.of(OUTPUT_FILES_LOCATION).resolve(file.getName()).toString()
+					+ SOCCER_TABLE_STATS_FILE_EXTENSION;
+			try (FileOutputStream outputStream = new FileOutputStream(destinationPath)) {
 				League league = new League();
 				Path path = Path.of(file.getAbsolutePath());
 				league.addGameResults(parseGameResults(Files.readString(path),
 						InputFormat.fromFileName(path.getFileName().toString())));
 				outputStream.write(league.toFormattedTable().getBytes());
+				LOGGER.log(Level.INFO, "Generated \"{0}\".", destinationPath);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
