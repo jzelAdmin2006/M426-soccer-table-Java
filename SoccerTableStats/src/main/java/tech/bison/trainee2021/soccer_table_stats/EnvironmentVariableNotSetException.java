8556cc4b888aced83e29a9a7d905c0b2e37938ef
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-06-25 10:27:38 +0200

    use static import as in other classes

--- SoccerTableStats/src/main/java/tech/bison/trainee2021/soccer_table_stats/EnvironmentVariableNotSetException.java
+++ SoccerTableStats/src/main/java/tech/bison/trainee2021/soccer_table_stats/EnvironmentVariableNotSetException.java
@@ -1,9 +1,11 @@
 package tech.bison.trainee2021.soccer_table_stats;
 
+import static java.lang.String.format;
+
 class EnvironmentVariableNotSetException extends IllegalArgumentException {
 	private static final long serialVersionUID = 8052329652437633248L;
 
 	EnvironmentVariableNotSetException(String variable) {
-		super(String.format("The environment variable \"%s\" was not set.", variable));
+		super(format("The environment variable \"%s\" was not set.", variable));
 	}
 }
